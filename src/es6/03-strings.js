e2aa1a41fb8390a5eedaa19f2a95d601b60b70fd
Patatatat <andres.guluarte@gmail.com>
2022-10-28 15:19:10 -0600

    03-strings.js

---
+++ src/es6/03-strings.js
@@ -0,0 +1,17 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+//console.log(epicPhrase);
+//Template literals
+
+let epicPhrase2 = ` ${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//multi-line strings
+
+let lorem = 'This is a string \n' + 'gnirts a si sihT';
+console.log(lorem);
+
+let lorem2 = `gnirts a si sihT
+This is a string`;
+console.log(lorem2);
