4526a83055ad32af4aafb6915977027631772c68
Patatatat <andres.guluarte@gmail.com>
2022-11-01 16:04:11 -0700

    spread and rest values

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,20 @@ console.log(b, d, a, c);
 let user = {userName: 'CarlosIV', age: 4};
 let {userName, age} = user;
 console.log(userName, age);
+
+//spread Operator
+
+let person = {name:'oscar', age: 28};
+let country = 'MX';
+let data = {id:1, ...person, country};
+console.log(data);
+
+//rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
