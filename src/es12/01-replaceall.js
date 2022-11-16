34ad881836da7b1d90ec73bfd56145639244b993
Patatatat <andres.guluarte@gmail.com>
2022-11-15 20:30:06 -0600

    00 && 01

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,5 @@
+const string = "JavaScript es un maravilloso lenguaje de programacion";
+
+const replacedString = string.replace("JavaScript", "JavaScript y Python");
+
+console.log(replacedString);
