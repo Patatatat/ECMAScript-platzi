ce0d4c2034a938c42e328db12647bb87814271b2
Patatatat <andres.guluarte@gmail.com>
2022-11-15 20:04:14 -0600

    es11

---
+++ src/es11/06-dynamic-import.js
@@ -0,0 +1,7 @@
+const boutton = document.getElementById('BTN');
+
+boutton.addEventListener("click", async function() {
+  const module = await import('./module.js');
+  console.log(module);
+  module.hello();
+})
